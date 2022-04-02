ed4aca8a8c224cce4aad8143942cf181c83a7579
Ali Raza Marchal <alirazamarchal@hotmail.com>
2022-04-02 20:38:32 +0500

    completed reports and finalized for submision

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -2,20 +2,54 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class ReportController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+
+
+        if ($request->has('date_range')) {
+            $datetime1 = null;
+            $datetime2 = null;
+
+            if (isset($request->date_range) && !empty($request->date_range)) {
+                $dates = explode(' – ', $request->date_range);
+                $fdate = @$dates[0];
+                $tdate = @$dates[1];
+                if (!empty($fdate) && !empty($tdate)) {
+                    $datetime1 = new \DateTime($fdate);
+                    $datetime2 = new \DateTime($tdate);
+                }
+            }
+
+            $date_from = null;
+            $date_to = null;
+
+            if (!empty($request->input('date_range'))) {
+                $date_from = $datetime1->format('Y-m-d');
+                $date_to = $datetime2->format('Y-m-d');
+            }
+
+
+            $data = DB::table('complaints')
+                ->select('office', 'category', DB::raw("count(category) as total"))
+                ->whereBetween('complaint_date', [$date_from, $date_to])
+                ->groupBy('category')
+                ->get();
+
+            return view('reports.index', compact('data', 'date_from', 'date_to'));
+        }
+
+
         $data = DB::table('complaints')
             ->select('office', 'category', DB::raw("count(category) as total"))
             ->groupBy('category')
             ->get();
 
-
-//        dd($data);
-        return view('reports.index',compact('data'));
+        return view('reports.index', compact('data'));
     }
 }
--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -27,28 +27,43 @@
             }
         </style>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Noto+Nastaliq+Urdu">
-    @endsection
-    <x-slot name="header">
-        <h2 class="animate-charcter font-bold font-semibold text-xl text-gray-800 leading-tight text-center">
-            Azad Jammu & Kashmir Electricity Department - Complaint Management System
-        </h2>
 
-        {{--        <div class="flex justify-center items-center float-right">--}}
-        {{--            <a href="{{route('category.create')}}" class="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-lg--}}
-        {{--            focus:outline-none hover:bg-gray-100 transition-colors duration-200 transform dark:text-gray-200--}}
-        {{--            dark:border-gray-200  dark:hover:bg-gray-700 2" title="Members List">--}}
-        {{--                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">--}}
-        {{--                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />--}}
-        {{--                </svg>--}}
-        {{--                <span class="hidden md:inline-block ml-2">Create New Category</span>--}}
-        {{--            </a>--}}
-        {{--        </div>--}}
+        <link rel="stylesheet" href="{{url('daterange/daterangepicker.min.css')}}">
+        <script src="{{url('daterange/jquery-3.6.0.min.js')}}"></script>
+        <script src="{{url('daterange/moment.min.js')}}"></script>
+        <script src="{{url('daterange/knockout-3.5.1.js')}}" defer></script>
+        <script src="{{url('daterange/daterangepicker.min.js')}}" defer></script>
+
+    @endsection
+    <x-slot name="header" style="height: 80px;">
+                    <h2 class="animate-charcter font-bold font-semibold text-xl text-gray-800 leading-tight text-center inline-block">
+                        Azad Jammu & Kashmir Electricity Department - Complaint Management System
+                    </h2>
+
+                    <form action="{{route('report.index')}}" method="get" class="float-right">
+                        <input  type="search" readonly name="date_range" id="date_range" >
+                        &nbsp;
+                        <input type="submit" value="Search" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring focus:ring-gray-300 disabled:opacity-25 transition">
+                    </form>
+
+{{--                    <a href="{{route('category.create')}}" class="flex items-center px-4 py-2 text-gray-600 bg-white border rounded-lg--}}
+{{--                    focus:outline-none hover:bg-gray-100 transition-colors duration-200 transform dark:text-gray-200--}}
+{{--                    dark:border-gray-200  dark:hover:bg-gray-700 2" title="Members List">--}}
+{{--                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">--}}
+{{--                            <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />--}}
+{{--                        </svg>--}}
+{{--                        <span class="hidden md:inline-block ml-2">Create New Category</span>--}}
+{{--                    </a>--}}
     </x-slot>
 
     <div class="py-22">
+
         <div class="max-w-7xl mx-auto sm:px-1 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5 mt-8">
+            <div class="bg-white overflow-hidden  sm:rounded-lg p-5 mt-8">
                 <h1 class="text-center text-2xl" style="font-family: 'Noto Nastaliq Urdu'; direction: rtl;">دفتر چیف انجینئر برقیات کو موصولہ درخواست/ شکایات / مکتوبات پر کی گئی کارروائی </h1>
+                @if(request()->has('date_range'))
+                <h1 class="text-center text-2xl" style="font-family: 'Noto Nastaliq Urdu'; direction: rtl;"> بابت {{$date_to}} تا {{$date_from}}</h1>
+                @endif
                 <br>
 
 
@@ -88,13 +103,13 @@
                         </thead>
                         <tbody>
 
-@php
-$first = 0;
-$second = 0;
-$third = 0;
-$fourth = 0;
-$fifth = 0;
-@endphp
+                    @php
+                    $first = 0;
+                    $second = 0;
+                    $third = 0;
+                    $fourth = 0;
+                    $fifth = 0;
+                    @endphp
                             @foreach(\App\Models\User::office() as $office)
                                 <tr class="bg-white border-b dark:bg-gray-800 dark:border-black text-center">
                                 <th scope="row" class="px-1 border border-black  py-2 font-medium text-black dark:text-white whitespace-nowrap">
@@ -170,17 +185,31 @@ $fifth = 0;
 
 
                                     <td class="px-1 py-2 border border-black">
-                                       0
+
+                                        @if(request()->has('date_range'))
+                                            {{\App\Models\Complaint::where('office',$office)->where('status','In Process')->whereBetween('complaint_date', [$date_from, $date_to])->count()}}
+                                        @else
+                                            {{\App\Models\Complaint::where('office',$office)->where('status','In Process')->count()}}
+                                        @endif
                                     </td>
                                 </tr>
                             @endforeach
-                            <tr class="text-center">
-                                <td class="px-1 py-2 border border-black">میزان</td>
+                            <tr class="text-center font-bold">
+                                <td class="px-1 pb-4 border border-black" style="font-family: 'Noto Nastaliq Urdu';">میزان</td>
                                 <td class="px-1 py-2 border border-black">{{$first}}</td>
                                 <td class="px-1 py-2 border border-black">{{$second}}</td>
                                 <td class="px-1 py-2 border border-black">{{$third}}</td>
                                 <td class="px-1 py-2 border border-black">{{$fourth}}</td>
                                 <td class="px-1 py-2 border border-black">{{$fifth}}</td>
+                                <td class="px-1 py-2 border border-black">{{$data->sum('total') }}</td>
+                                <td class="px-1 py-2 border border-black">
+                                    @if(request()->has('date_range'))
+                                        {{\App\Models\Complaint::where('status','In Process')->whereBetween('complaint_date', [$date_from, $date_to])->count()}}
+                                    @else
+                                        {{\App\Models\Complaint::where('status','In Process')->count()}}
+                                    @endif
+
+                                </td>
                             </tr>
 
 
@@ -191,4 +220,17 @@ $fifth = 0;
             </div>
         </div>
     </div>
+    @section('custom_footer')
+            <script>
+                $(document).ready(function(){
+                    $("#date_range").daterangepicker({
+                        minDate: moment().subtract(10, 'years'),
+                        orientation:'left',
+                        callback: function (startDate, endDate, period) {
+                            $(this).val(startDate.format('L') + ' – ' + endDate.format('L'));
+                        }
+                    });
+                });
+            </script>
+    @endsection
 </x-app-layout>
